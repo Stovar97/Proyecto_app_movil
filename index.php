2d6babe5d975f9b0e7d1f6f21662ed2b6fb7f51f
charly <lancheros396@gmail.com>
2019-04-13 18:01:48 -0500

    hola gays

--- index.php
+++ index.php
@@ -13,25 +13,8 @@
   <body>
    <div class header>
    </div>
+   <?php include_once='menu.php';?>
    <br>  
-   <div class nav>
-      <div class="container">          
-          <ul class="nav nav-pills">
-              <li class="nav-item">
-                <a class="nav-link active" href="index.html">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Quienes Somos</a>
-              </li>  
-              <li class="nav-item">
-                 <a class="nav-link" href="#">Registrarse</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="contactenos.html">Contactenos</a>
-               </li>                            
-          </ul>
-          </div>  
-          </div>
     <br>    
      <br>
      <br>   
